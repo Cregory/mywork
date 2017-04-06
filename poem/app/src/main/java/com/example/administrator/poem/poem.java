8d136b14ada98fbedf714b26a45cccc73d8ff7d2
Cregory <as467831613@126.com>
2017-04-06 15:10:02 +0800

    布局的作业

---
+++ poem/app/src/main/java/com/example/administrator/poem/poem.java
@@ -0,0 +1,13 @@
+package com.example.administrator.poem;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class poem extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_poem);
+    }
+}
